334013529adedb2ae8697e9a1f7b94663804db9b
Steph Skardal <steph@endpoint.com>
2012-01-29 11:35:28 -0500

    Minor cleanup.

--- app/controllers/rails_admin_import/base_controller.rb
+++ app/controllers/rails_admin_import/base_controller.rb
@@ -1,4 +1,6 @@
 class RailsAdminImport::BaseController < RailsAdmin::MainController
+  layout 'rails_admin/application'
+
   def import
     @response = {}
 
@@ -6,17 +8,6 @@ class RailsAdminImport::BaseController < RailsAdmin::MainController
     @response[:notice] = results[:success].join("<br />").html_safe if results[:success].any?
     @response[:error] = results[:error].join("<br />").html_safe if results[:error].any?
 
-    # TODO: Gross
-    # * Can't use flash because cookie store 4kb method will likely be hit
-    # * Can't redirect to rails_admin.import because we lose @response
-    # * Can't call RailsAdmin::MainController.import because it's private
-    # * So, method copied directly below
     @action = RailsAdmin::Config::Actions.find(:import, {:controller => self, :abstract_model => @abstract_model, :object => @object})
-          
-    @authorization_adapter.try(:authorize, @action.authorization_key, @abstract_model, @object)
-    @page_name = wording_for(:title)
-    @page_type = @abstract_model && @abstract_model.pretty_name.downcase || "dashboard"
-          
-    instance_eval &@action.controller
   end
 end
