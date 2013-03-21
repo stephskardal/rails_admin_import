3f49e7e642c429f6ea1653cd10ca1d74c7f43439
adamwong246 <adam@spaceport.io>
2013-03-20 20:36:34 -0700

    refactored code to keep params out of the model. File type is set explicitly now.

--- lib/rails_admin_import.rb
+++ lib/rails_admin_import.rb
@@ -41,36 +41,61 @@ module RailsAdmin
         register_instance_option :controller do
           Proc.new do
             @response = {}
-            text = ""
 
-            # @abstract_model -> Post
-            # @abstract_model.model -> Post(id: integer, title: string, slug: string, body: text, body_html: text, active: boolean, approved_comments_count: integer, cached_tag_list: string, published_at: datetime, created_at: datetime, updated_at: datetime, edited_at: datetime)
-            
             # make sure class has import-related methods
             @abstract_model.model.send :include, ::RailsAdminImport::Import
 
             if request.post?
-              
               if !params.has_key?(:file)
                 return results = { :success => [], :error => ["You must select a file."] }
-
-              elsif params[:file].is_a? String
-                # text = open(params[:file]).read()
-
-                results = @abstract_model.model.rss_import(params, _attr_accessible_role, _current_user)
-
-              elsif params[:file].is_a? ActionDispatch::Http::UploadedFile
-                # text = File.read(params[:file].tempfile)
-
-                if params[:file].content_type == "text/csv"
-                  results = @abstract_model.model.csv_import(params, _attr_accessible_role, _current_user)
-                end
               end
 
-              # results             = @abstract_model.model.run_import(params, _attr_accessible_role, _current_user)
+              associated_map = {}
+
+              @abstract_model.model.belongs_to_fields.flatten.each do |field|
+                associated_map[field] = field.to_s.classify.constantize.all.inject({}) { |hash, c| hash[c.send(params[field]).to_s] = c.id; hash }
+              end
+              
+              @abstract_model.model.many_fields.flatten.each do |field|
+                associated_map[field] = field.to_s.classify.constantize.all.inject({}) { |hash, c| hash[c.send(params[field]).to_s] = c; hash }
+              end
+
+              results = @abstract_model.model.run_import(
+                params[:file], 
+                params[:update_lookup],
+                associated_map,
+                params[:type].to_sym,
+                _attr_accessible_role, 
+                _current_user
+              )
+
+
+
               @response[:notice]  = results[:success].join("<br />").html_safe  if results[:success].any?
               @response[:error]   = results[:error].join("<br />").html_safe    if results[:error].any?
             end
+
+            # if request.post?
+              
+              # if !params.has_key?(:file)
+              #   return results = { :success => [], :error => ["You must select a file."] }
+
+
+              # elsif params[:file].is_a? String
+
+              #   results = @abstract_model.model.rss_import(params, _attr_accessible_role, _current_user)
+
+              # elsif params[:file].is_a? ActionDispatch::Http::UploadedFile
+
+              #   if params[:file].content_type == "text/csv"
+              #     results = @abstract_model.model.csv_import(params, _attr_accessible_role, _current_user)
+              #   end
+              # end
+
+              # results             = @abstract_model.model.run_import(params, _attr_accessible_role, _current_user)
+              # @response[:notice]  = results[:success].join("<br />").html_safe  if results[:success].any?
+              # @response[:error]   = results[:error].join("<br />").html_safe    if results[:error].any?
+            # end
 
             render :action => @action.template_name
           end
--- lib/rails_admin_import/import.rb
+++ lib/rails_admin_import/import.rb
@@ -63,79 +63,49 @@ module RailsAdminImport
         associations = self.respond_to?(:relations) ? self.relations : self.reflections
       end
 
-      def rss_import(params, role, current_user)
+      def run_import(param_file, param_update_lookup_field = nil, associated_map = nil, file_type, role, user)
 
-        # TODO: 
-        # if RailsAdminImport.config.logging
-        #   FileUtils.copy(params[:file].tempfile, "#{Rails.root}/log/import/#{Time.now.strftime("%Y-%m-%d-%H-%M-%S")}-import.rss")
-        #   logger = Logger.new("#{Rails.root}/log/import/import.log")
-        # end
+        if (param_file.is_a? String) 
+          temp_file =  open(param_file)
+        elsif param_file.is_a? ActionDispatch::Http::UploadedFile
+          temp_file = param_file.tempfile
+        else
+          return { :success => [], :error => ["Couldn't parse your file."] }
+        end
 
-        # text        = File.read(params[:file].tempfile)
-        # text = open(params[:file]) { |f| f.read }
-        # clean       = text.force_encoding('BINARY').encode('UTF-8', :undef => :replace, :replace => '').gsub(/\n$/, '')
-        # file_check  = CSV.new(clean)
+        if RailsAdminImport.config.logging
+          FileUtils.copy(temp_file, "#{Rails.root}/log/import/#{Time.now.strftime("%Y-%m-%d-%H-%M-%S")}-import.csv")
+          logger = Logger.new("#{Rails.root}/log/import/import.log")
+        end
 
-        rss = Feedzirra::Feed.fetch_and_parse(params[:file])
+        if import_config.update_lookup_field
+          lookup_field_name = import_config.update_lookup_field
+        elsif !param_update_lookup_field.blank?
+          lookup_field_name = param_update_lookup_field.to_sym
+        end
 
+        if file_type == :csv
+          return csv_import(temp_file, lookup_field_name, associated_map, role, user)
+        elsif file_type == :rss
+          return rss_import(temp_file, lookup_field_name, role, user)
+        else
+          return { :success => [], :error => ["Could not recognize the file type: #{file_type.to_s}"] }
+        end
+
+      end
+
+      def rss_import(temp_file, lookup_field_name, role, current_user)
+
+        rss = Feedzirra::Feed.parse(File.read(temp_file))
 
         # if file_check.readlines.size > RailsAdminImport.config.line_item_limit
         if rss.entries.length > RailsAdminImport.config.line_item_limit
           return results = { :success => [], :error => ["Please limit upload file to #{RailsAdminImport.config.line_item_limit} line items."] }
         end
-
-        # Creates a mapping 
-        # TODO: implement in a generic way
-        ###########
-        # map         = HashWithIndifferentAccess.new {}
-        # # file        = CSV.new(clean)
-        
-        # file.readline.each_with_index do |key, i|
-        #   if self.many_fields.include?(key.to_sym)
-        #     map[key.downcase.to_sym] ||= []
-        #     map[key.downcase.to_sym] << i
-        #   else
-        #     map[key.downcase.to_sym] = i 
-        #   end
-        # end
-        
-        # if import_config.update_lookup_field
-        #   lookup_field_name = import_config.update_lookup_field
-        # elsif !params[:update_lookup].blank?
-        #   lookup_field_name = params[:update_lookup].to_sym
-        # end
-
-        # if lookup_field_name && !map.has_key?(lookup_field_name)
-        #   return results = { 
-        #     :success => [], 
-        #     :error => [
-        #       "Your file must contain a column for the 'Update lookup field' you selected.", 
-        #       "The current 'Upate lookup field' is '#{lookup_field_name}'.", 
-        #       "The columns found were #{map.keys}"
-        #     ] 
-        #   }
-        # end 
   
         results = { :success => [], :error => [] }
-        # associated_map = {}
-        
-        # self.belongs_to_fields.flatten.each do |field|
-        #   associated_map[field] = field.to_s.classify.constantize.all.inject({}) { |hash, c| hash[c.send(params[field]).to_s] = c.id; hash }
-        # end
-        
-        # self.many_fields.flatten.each do |field|
-        #   associated_map[field] = field.to_s.classify.constantize.all.inject({}) { |hash, c| hash[c.send(params[field]).to_s] = c; hash }
-        # end
- 
         label_method        = import_config.label
         # before_import_save  = import_config.before_import_save
-        
-        # # handle nesting in parent object
-        # if import_config.create_parent
-        #   parent_object = import_config.create_parent.call(role, current_user)
-        #   nested_field  = import_config.nested_field
-        # end
-        #######
         
         # for each of the items in the rss file...
         rss.entries.each do |entry|
@@ -143,34 +113,11 @@ module RailsAdminImport
           # Excute the Procs in rss_mapping with the entry from the rss item
           new_attrs = Hash[import_config.rss_mapping.map{ |k, v| [k, v.call(entry)] }]
           
-          # self.import_fields.each do |key|
-          #   new_attrs[key] = row[map[key]] if map[key]
-          # end
-          
-          # lookup_field_value = row[map[lookup_field_name]] unless lookup_field_name.blank?
-          
           object = self.import_initialize(new_attrs, :title, new_attrs[:title])
-          # object.import_belongs_to_data(associated_map, row, map)
-          # object.import_many_data(associated_map, row, map)
-          
-          # if before_import_save
-          #   before_import_save_args = [object, row, map, role, current_user]
-          #   before_import_save_args << parent_object if parent_object
-            
-          #   callback_result         = before_import_save.call(*before_import_save_args)
-          #   skip_nested_save        = callback_result == false && !parent_object.nil?
-          # end
-          
-          # object.import_files(row, map)
-          
-          # if parent_object
-          #   parent_object.send(nested_field) << object
-          # end
+
           skip_nested_save = false
 
           verb = object.new_record? ? "Create" : "Update"
-
-          # debugger
           
           if object.errors.empty?
             if skip_nested_save
@@ -192,21 +139,6 @@ module RailsAdminImport
             results[:error] << "Errors before save: #{object.send(label_method)}. Errors: #{object.errors.full_messages.join(', ')}."
           end
         end
-        
-        # if parent_object
-        #   import_config.before_parent_save.call(parent_object, role, current_user) if import_config.before_parent_save
-          
-        #   if parent_object.save
-        #     logger.info "#{Time.now.to_s}: Saved #{parent_object.class.name}" if RailsAdminImport.config.logging
-        #     results[:success].unshift "Saved: #{parent_object}"
-        #   else
-        #     logger.info "#{Time.now.to_s}: Failed to save #{parent_object.class.name}. Errors: #{parent_object.errors.full_messages.join(', ')}." if RailsAdminImport.config.logging
-        #     results[:error].unshift "Failed to save #{parent_object.class.name}. Errors: #{parent_object.errors.full_messages.join(', ')}."
-        #   end
-          
-        #   import_config.after_parent_save.call(parent_object, role, current_user) if import_config.after_parent_save
-          
-        # end
   
         results
       # rescue Exception => e
@@ -215,15 +147,8 @@ module RailsAdminImport
       # end
     end
 
-      def csv_import(params, role, current_user)
-        # begin csv import
-
-        if RailsAdminImport.config.logging
-          FileUtils.copy(params[:file].tempfile, "#{Rails.root}/log/import/#{Time.now.strftime("%Y-%m-%d-%H-%M-%S")}-import.csv")
-          logger = Logger.new("#{Rails.root}/log/import/import.log")
-        end
-
-        text        = File.read(params[:file].tempfile)
+      def csv_import(temp_file, lookup_field_name, associated_map, role, current_user)
+        text        = File.read(temp_file)
         clean       = text.force_encoding('BINARY').encode('UTF-8', :undef => :replace, :replace => '').gsub(/\n$/, '')
         file_check  = CSV.new(clean)
 
@@ -243,11 +168,11 @@ module RailsAdminImport
           end
         end
         
-        if import_config.update_lookup_field
-          lookup_field_name = import_config.update_lookup_field
-        elsif !params[:update_lookup].blank?
-          lookup_field_name = params[:update_lookup].to_sym
-        end
+        # if import_config.update_lookup_field
+        #   lookup_field_name = import_config.update_lookup_field
+        # elsif !params[:update_lookup].blank?
+        #   lookup_field_name = params[:update_lookup].to_sym
+        # end
 
         if lookup_field_name && !map.has_key?(lookup_field_name)
           return results = { 
@@ -261,15 +186,15 @@ module RailsAdminImport
         end 
   
         results = { :success => [], :error => [] }
-        associated_map = {}
+        # associated_map = {}
         
-        self.belongs_to_fields.flatten.each do |field|
-          associated_map[field] = field.to_s.classify.constantize.all.inject({}) { |hash, c| hash[c.send(params[field]).to_s] = c.id; hash }
-        end
+        # self.belongs_to_fields.flatten.each do |field|
+        #   associated_map[field] = field.to_s.classify.constantize.all.inject({}) { |hash, c| hash[c.send(params[field]).to_s] = c.id; hash }
+        # end
         
-        self.many_fields.flatten.each do |field|
-          associated_map[field] = field.to_s.classify.constantize.all.inject({}) { |hash, c| hash[c.send(params[field]).to_s] = c; hash }
-        end
+        # self.many_fields.flatten.each do |field|
+        #   associated_map[field] = field.to_s.classify.constantize.all.inject({}) { |hash, c| hash[c.send(params[field]).to_s] = c; hash }
+        # end
  
         label_method        = import_config.label
         before_import_save  = import_config.before_import_save
