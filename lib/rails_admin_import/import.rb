dc8b9a1f9cf38a2224933d5b1a05fc7fd85e28c8
Tao Wang <moonavw@gmail.com>
2014-11-05 12:18:19 +0800

    fix issue#27
    https://github.com/stephskardal/rails_admin_import/issues/27

--- lib/rails_admin_import/import.rb
+++ lib/rails_admin_import/import.rb
@@ -65,7 +65,7 @@ module RailsAdminImport
           end
 
           text       = File.read(params[:file].tempfile)
-          clean      = text.force_encoding('BINARY').encode('UTF-8', :undef => :replace, :replace => '').gsub(/\n$/, '')
+          clean      = text #.force_encoding('BINARY').encode('UTF-8', :undef => :replace, :replace => '').gsub(/\n$/, '')
           file_check = CSV.new(clean)
           logger     = ImportLogger.new
      
