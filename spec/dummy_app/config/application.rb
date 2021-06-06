7845594f27493d7e5909f80245d36c21c3eefe25
Julien Vanier <jvanier@gmail.com>
2021-06-06 17:37:53 -0400

    Fix deprecation errors

--- spec/dummy_app/config/application.rb
+++ spec/dummy_app/config/application.rb
@@ -22,8 +22,6 @@ module DummyApp
     config.eager_load_paths.reject! { |p| p =~ %r{/app/(\w+)$} && !%w(controllers helpers views).push(CI_ORM).include?(Regexp.last_match[1]) }
     config.autoload_paths += %W(#{config.root}/app/#{CI_ORM} #{config.root}/app/#{CI_ORM}/concerns)
 
-    config.active_record.sqlite3.represent_boolean_as_integer = true if defined?(SQLite3)
-
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
